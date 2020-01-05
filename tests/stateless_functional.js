bc31fd672fb367d938a4ace4e89c2bcd87716797
Chris Neale <chris@usablehq.com>
2020-01-05 11:57:34 +0000

    SFC function prop test

--- tests/stateless_functional.js
+++ tests/stateless_functional.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Example({ style, string="String", update=()=>{} }, id, count=123) {
+export default function Example({ style, string="String", update=()=>{} }, id, count=123, save=()=>{}) {
   return (
     <div className="example">
       <button onClick={() => {}}>Vertical (Left)</button>
