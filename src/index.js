e330bbbfc312fa8c94faabc217d43cb1efb0fc7c
Chris Neale <chris@usablehq.com>
2020-01-24 11:11:44 +0000

    Handling shape prop

--- src/index.js
+++ src/index.js
@@ -9,88 +9,91 @@ const config = require("./narrator.config.json");
 const { Exports, ExportDefault, IdentifierName, CalleeName } = require("./lib/Extractors");
 const { find, findExpressionPropTypes, findClassPropTypes, declarationParamsToObject } = require("./lib/AST");
 const { propTypesToObject } = require("./lib/propTypesToObject");
+try {
+  glob(config.src, {}, function(err, files) {
+    if (err) { console.log(err); }
+    const reports = [];
 
-glob(config.src, {}, function(err, files) {
-  if (err) { console.log(err); }
-  const reports = [];
+    files.map(file => {
 
-  files.map(file => {
+      const contents = fs.readFileSync(file, "utf8");
 
-    const contents = fs.readFileSync(file, "utf8");
+      let pt = {};
 
-    let pt = {};
+      const b = babelParser.parse(contents, {
+        sourceType: "module",
+        plugins: config.babel.plugins
+      });
 
-    const b = babelParser.parse(contents, {
-      sourceType: "module",
-      plugins: config.babel.plugins
-    });
+      const exportDefault = ExportDefault.evaluate(b);
 
-    const exportDefault = ExportDefault.evaluate(b);
-
-    if (exportDefault) {
-      if (exportDefault.declaration.type === "Identifier") {
-        const identifierName = IdentifierName.evaluate(exportDefault);
-        try {
-          const x = find(b, identifierName);
-          if (!x) return;
-          def("Export Def I", x, identifierName)
-          if (x.type === "ClassDeclaration") {
-            // proptypes might be defined as external expression statements
-            pt = propTypesToObject(findClassPropTypes(x, identifierName), b);
-            if (!Object.keys(pt).length) {
+      if (exportDefault) {
+        if (exportDefault.declaration.type === "Identifier") {
+          const identifierName = IdentifierName.evaluate(exportDefault);
+          try {
+            const x = find(b, identifierName);
+            if (!x) return;
+            def("Export Def I", x, identifierName)
+            if (x.type === "ClassDeclaration") {
+              // proptypes might be defined as external expression statements
+              pt = propTypesToObject(findClassPropTypes(x, identifierName), b);
+              if (!Object.keys(pt).length) {
+                if (identifierName) {
+                  pt = propTypesToObject(findExpressionPropTypes(b, identifierName), b);
+                }
+              }
+            } else if (x.type === "VariableDeclaration") {
               if (identifierName) {
-                pt = propTypesToObject(findExpressionPropTypes(b, identifierName), b);
+                pt = propTypesToObject(findExpressionPropTypes(b, identifierName));
               }
             }
-          } else if (x.type === "VariableDeclaration") {
-            if (identifierName) {
-              pt = propTypesToObject(findExpressionPropTypes(b, identifierName));
+            // console.log("Props".padEnd(15), JSON.stringify(pt, null, 2));
+          } catch(error) {
+            console.log(error);
+          }
+        } else if (exportDefault.declaration.type === "CallExpression") {
+          const CallExpressionName = CalleeName.evaluate(exportDefault);
+          // console.log("Export Def CE".padEnd(15), "(Function)", CallExpressionName ? CallExpressionName.padStart(5) : "Anonymous" );
+          if (CalleeName.evaluate(exportDefault) === 'connect') {
+            const identifierName = exportDefault.declaration.arguments[0].name;
+            const x = find(b, identifierName);
+            def("Callee arg 0", x, identifierName);
+            if (x !== null) {
+              if (x.type === "ClassDeclaration") {
+                pt = propTypesToObject(findClassPropTypes(x));
+              } else if (x.type === "VariableDeclaration") {
+                pt = propTypesToObject(findExpressionPropTypes(b, identifierName));
+              }
             }
           }
-          // console.log("Props".padEnd(15), JSON.stringify(pt, null, 2));
-        } catch(error) {
-          console.log(error);
+        } else if (exportDefault.declaration.type === "FunctionDeclaration") {
+          pt = declarationParamsToObject(exportDefault.declaration);
+        } else {
+          // console.log("Export Default".padEnd(15), "(Something Else)", exportDefault.declaration.type );
         }
-      } else if (exportDefault.declaration.type === "CallExpression") {
-        const CallExpressionName = CalleeName.evaluate(exportDefault);
-        // console.log("Export Def CE".padEnd(15), "(Function)", CallExpressionName ? CallExpressionName.padStart(5) : "Anonymous" );
-        if (CalleeName.evaluate(exportDefault) === 'connect') {
-          const identifierName = exportDefault.declaration.arguments[0].name;
-          const x = find(b, identifierName);
-          def("Callee arg 0", x, identifierName);
-          if (x !== null) {
-            if (x.type === "ClassDeclaration") {
-              pt = propTypesToObject(findClassPropTypes(x));
-            } else if (x.type === "VariableDeclaration") {
-              pt = propTypesToObject(findExpressionPropTypes(b, identifierName));
+      }
+
+      const exps = Exports.evaluate(b);
+      if (exps) {
+        if (exps.length) {
+          exps.map(
+            (exp) => {
+              // callInterogator(exp.declaration.declarations[0]);
             }
-          }
+          )
+        } else {
+          // callInterogator(exps.declaration.declarations[0]);
         }
-      } else if (exportDefault.declaration.type === "FunctionDeclaration") {
-        pt = declarationParamsToObject(exportDefault.declaration);
-      } else {
-        // console.log("Export Default".padEnd(15), "(Something Else)", exportDefault.declaration.type );
       }
-    }
 
-    const exps = Exports.evaluate(b);
-    if (exps) {
-      if (exps.length) {
-        exps.map(
-          (exp) => {
-            // callInterogator(exp.declaration.declarations[0]);
-          }
-        )
-      } else {
-        // callInterogator(exps.declaration.declarations[0]);
-      }
-    }
+      reports.push({ file, b, pt })
 
-    reports.push({ file, b, pt })
+    });
 
-  });
+    console.log("Report:");
+    reporter(reports);
 
-  // console.log("Report:");
-  // reporter(reports);
-
-});
+  })
+} catch(error) {
+  console.log(error);
+};
--- src/lib/propTypesToObject.js
+++ src/lib/propTypesToObject.js
@@ -24,23 +24,28 @@ function parsePropChain(proptype) {
               } 
             });
           }
-            if (prop.callee.hasOwnProperty("property")) {
-              chain.push(`${prop.callee.property.name}[${args.join(',')}]`);
-            } else {
-              chain.push(`${prop.callee.name}[${args.join(',')}]`);
-            }
-          prop = prop.callee.object;
+          if (prop.callee.hasOwnProperty("property")) {
+            chain.push(`${prop.callee.property.name}[${args.join(',')}]`);
+          } else {
+            chain.push(`${prop.callee.name}[${args.join(',')}]`);
+          }
+          if (prop.callee.hasOwnProperty('object')) {
+            prop = prop.callee.object;
+          } else {
+            prop = false;
+          }
         }
       }
       chain.push(prop.name)
     }
   } catch(e) {
+    console.error("Failed to parse prop chain.", proptype);
     process.exit(1);
   }
   return chain;
 }
 
-const propTypesToObject = ({ pt, pd }, b) => {
+const propTypesToObject = ({ pt, pd }, b, file=null) => {
 
   const propTypes = propTypesProperties.evaluate(pt);
   const defaultProps = defaultPropsProperties.evaluate(pd);
@@ -74,7 +79,6 @@ const propTypesToObject = ({ pt, pd }, b) => {
           break;
         case "Identifier":
           const x = find(b, prop.key.name)
-          // console.log(prop.key.name, x.type)
           if (typeof x === 'object' && x !== null) {
             switch (x.type) {
               case "ImportDeclaration":
@@ -85,7 +89,11 @@ const propTypesToObject = ({ pt, pd }, b) => {
                 break;
               }  
           } else {
-            console.log("Identifier is null");
+            if (x === null) {
+              props[prop.key.name].value = "NULL";
+            } else {
+              props[prop.key.name].value = x;
+            }
           }
           break;
         case "StringLiteral":
