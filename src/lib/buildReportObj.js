52d106d7326eb83cba468f2a5c5ac74b18558fc7
Chris Neale <chris@usablehq.com>
2020-02-04 10:42:25 +0000

    Fixed long lines

--- src/lib/buildReportObj.js
+++ src/lib/buildReportObj.js
@@ -1,61 +1,59 @@
-const { IdentifierName, CalleeName } = require("./Extractors");
+const { IdentifierName, CalleeName } = require('./Extractors');
 const {
   find,
   findExpressionPropTypes,
   findClassPropTypes,
-  declarationParamsToObject
-} = require("./AST");
-const { propTypesToObject } = require("./propTypesToObject");
+  declarationParamsToObject,
+} = require('./AST');
+const { propTypesToObject } = require('./propTypesToObject');
 
 const buildReportObj = (node, narrator) => {
   let pt = {};
   const parsedJs = narrator.b;
   if (node) {
-    if (node.declaration.type === "ClassDeclaration") {
+    if (node.declaration.type === 'ClassDeclaration') {
       const x = node.declaration;
 
-      console.log(narrator);
-
       pt = propTypesToObject(narrator.findPropTypes(x), parsedJs);
-      console.log("pt", pt);
+      console.log('pt', pt);
       if (!Object.keys(pt).length) {
         // const identifierName = IdentifierName.evaluate(node);
         // if (identifierName) {
         //   pt = propTypesToObject(findExpressionPropTypes(parsedJs, identifierName), parsedJs);
         // }
       }
-      return { name: "", pt };
-    } else if (node.declaration.type === "VariableDeclaration") {
+      return { name: '', pt };
+    } if (node.declaration.type === 'VariableDeclaration') {
       const identifierName = IdentifierName.evaluate(node);
       if (identifierName) {
         pt = propTypesToObject(
-          findExpressionPropTypes(parsedJs, identifierName)
+          findExpressionPropTypes(parsedJs, identifierName),
         );
       }
-      return { name: "", pt };
-    } else if (node.declaration.type === "Identifier") {
+      return { name: '', pt };
+    } if (node.declaration.type === 'Identifier') {
       /**
        * Find the ident. If there isn't one, anon export?
        */
       const identifierName = IdentifierName.evaluate(node);
       try {
         const x = find(parsedJs, identifierName);
-        if (!x) return { name: "", pt };
-        if (x.type === "ClassDeclaration") {
+        if (!x) return { name: '', pt };
+        if (x.type === 'ClassDeclaration') {
           // proptypes might be defined as external expression statements
           pt = propTypesToObject(findClassPropTypes(x), parsedJs);
           if (!Object.keys(pt).length) {
             if (identifierName) {
               pt = propTypesToObject(
                 findExpressionPropTypes(parsedJs, identifierName),
-                parsedJs
+                parsedJs,
               );
             }
           }
-        } else if (x.type === "VariableDeclaration") {
+        } else if (x.type === 'VariableDeclaration') {
           if (identifierName) {
             pt = propTypesToObject(
-              findExpressionPropTypes(parsedJs, identifierName)
+              findExpressionPropTypes(parsedJs, identifierName),
             );
           }
         }
@@ -63,33 +61,37 @@ const buildReportObj = (node, narrator) => {
         console.log(error);
       }
       return { name: identifierName, pt };
-    } else if (node.declaration.type === "CallExpression") {
-      const CallExpressionName = CalleeName.evaluate(node);
-      if (CalleeName.evaluate(node) === "connect") {
+    } if (node.declaration.type === 'CallExpression') {
+      // const CallExpressionName = CalleeName.evaluate(node);
+      if (CalleeName.evaluate(node) === 'connect') {
         const identifierName = node.declaration.arguments[0].name;
         const x = find(parsedJs, identifierName);
         // def("Callee arg 0", x, identifierName);
         if (x !== null) {
-          if (x.type === "ClassDeclaration") {
+          if (x.type === 'ClassDeclaration') {
             pt = propTypesToObject(findClassPropTypes(x));
-          } else if (x.type === "VariableDeclaration") {
+          } else if (x.type === 'VariableDeclaration') {
             pt = propTypesToObject(
-              findExpressionPropTypes(parsedJs, identifierName)
+              findExpressionPropTypes(parsedJs, identifierName),
             );
           }
         }
         return { name: identifierName, pt };
       }
-    } else if (node.declaration.type === "FunctionDeclaration") {
+    } else if (node.declaration.type === 'FunctionDeclaration') {
       const identifierName = node.declaration.id
         ? node.declaration.id.name
-        : "Anon";
+        : 'Anon';
       pt = declarationParamsToObject(node.declaration);
       return { name: identifierName, pt };
     } else {
-      // console.log("Export Default".padEnd(15), "(Something Else)", exportDefault.declaration.type );
+      // console.log("Export Default".padEnd(15),
+      //   "(Something Else)",
+      //   exportDefault.declaration.type);
     }
   }
+
+  return true;
 };
 
-module.exports = { buildReportObj: buildReportObj };
+module.exports = { buildReportObj };
